beaa943852d608a2d8ee33ce0b5cc41e56bc085b
Brad Armstrong <bradlarmstrong@gmail.com>
2021-04-16 14:06:04 -0500

    changed file name

---
+++ makefile_lib/utils/before-nco-uninstall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LOG=/tmp/nms_uninstall.log
+BASEDIR=/opt/viasat/deployments/makefile_lib
+
+#Determine System Role
+SYSTEM_HOSTNAME=`hostname | cut -d"." -f1`
+SYSTEM_ROLE=`cat ${BASEDIR}/utils/hostmanifest | grep ${SYSTEM_HOSTNAME} | cut -f2`
+PA_NAME=`cat ${BASEDIR}/utils/hostmanifest | grep ${SYSTEM_HOSTNAME} | cut -f4` 
+echo "System Role is ${SYSTEM_ROLE}.  PA_NAME is ${PA_NAME}"
+
+
+#run installer as root
+echo "Installing Netcool Omnibus"
+echo "Check $LOG for more info"
+make -f $BASEDIR/products/omnibus_core_8_1_0_19.mk uninstall PATH_MAKEFILE_MEDIA=/opt/viasat/nms/ibm/software NETCOOL_PA_NAME=${PA_NAME} > $LOG
